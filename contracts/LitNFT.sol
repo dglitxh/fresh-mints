4ab6ec685d958fe800c3d2ff88efd38b01f5b9fc
YD <ydglitch@gmail.com>
2022-04-14 13:47:27 +0000

    contract: minter-fxn

--- contracts/LitNFT.sol
+++ contracts/LitNFT.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract LitNFT {
-
-    constructor() {
+contract LitNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    constructor() ERC721("dzzyNFT", "dzzy") {
         console.log("I'm a lit NFT");
+    }
+
+    function makeLitNft() public {
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, "deezlytok")
+
+        tokenIds.increment();
     }
 }
