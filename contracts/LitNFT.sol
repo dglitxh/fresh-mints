ccb5f7a65466f8817173365c83f7ae6fcc6f98c5
YD <ydglitch@gmail.com>
2022-04-16 12:01:20 +0000

    fix: LitNFT

--- contracts/LitNFT.sol
+++ contracts/LitNFT.sol
@@ -18,7 +18,7 @@ contract LitNFT is ERC721URIStorage {
 
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/WUNK");
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgICAgICAgICAibmFtZSI6IkJ1Z3MgYnVubnkiLAogICAgICAgICAgICAiZGVzY3JpcHRpb24iOiJHZW5pdXMgYnVubnkiLAogICAgICAgICAgICAiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSMjlrSjNOR2JIbGxjM1JUYjI0OEwzUmxlSFErQ2p3dmMzWm5QZz09IgogICAgICAgICAgICB9");
 
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
         
